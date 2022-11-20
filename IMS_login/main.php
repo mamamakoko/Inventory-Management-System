1dbd324b7b21733c7ccb86dc86f3942801c25d66
hfuribun <118652698+hfuribun@users.noreply.github.com>
2022-11-20 16:44:42 +0800

    Add files via upload

---
+++ IMS_login/main.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Main</title>
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+	<div class="wrapper">
+			
+		<div class="center">
+			<h2>Inventory Management System</h2>
+			<a class="button" href="index.php">Login</a>
+			
+		</div>
+</body>
+</html>
