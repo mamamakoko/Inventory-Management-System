f5a14b156f22d42a2ac9c5f2552f31cd924d283a
John Francis Lomeda <jolomeda@my.cspc.edu.ph>
2022-11-22 21:14:00 +0800

    Added new source code (i.e., inventory)

--- IMS_login/main.php
+++ IMS_login/main.php
@@ -7,11 +7,15 @@
 </head>
 <body>
 	<div class="wrapper">
-			
 		<div class="center">
 			<h2>Inventory Management System</h2>
 			<a class="button" href="index.php">Login</a>
-			
+			<button type="button" id="register-button">Create Account</button>
 		</div>
+	</div>
+
+	<script>
+		var 
+	</script>
 </body>
 </html>
