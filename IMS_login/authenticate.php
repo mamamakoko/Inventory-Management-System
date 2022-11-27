45e2d254ef320eec710d39a7424ed748dd8ee531
John Francis Lomeda <jolomeda@my.cspc.edu.ph>
2022-11-27 13:44:10 +0800

    Kokoy added page for inventory

--- IMS_login/authenticate.php
+++ IMS_login/authenticate.php
@@ -3,23 +3,23 @@ session_start();
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'ims_login';
+$DATABASE_NAME = 'ims';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 
-if ( mysqli_connect_errno() ) {
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+if (mysqli_connect_errno()) {
+    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-if ( !isset($_POST['username'], $_POST['password']) ) {
-	exit('Please fill both the username and password fields!');
+if (!isset($_POST['username'], $_POST['password'])) {
+    exit('Please fill both the username and password fields!');
 }
 
 if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
-	$stmt->bind_param('s', $_POST['username']);
-	$stmt->execute();
+    // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+    $stmt->bind_param('s', $_POST['username']);
+    $stmt->execute();
 
-	$stmt->store_result();
+    $stmt->store_result();
 
     if ($stmt->num_rows > 0) {
         $stmt->bind_result($id, $password);
@@ -37,7 +37,5 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
         echo 'Incorrect username and/or password!';
     }
 
-	$stmt->close();
+    $stmt->close();
 }
-?>
-
