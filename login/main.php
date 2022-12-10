d85eb097b61ef34251ae18e78a908f4a259af900
hfuribun <118652698+hfuribun@users.noreply.github.com>
2022-12-10 20:39:27 +0800

    Add files via upload

---
+++ login/main.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Main</title>
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+	<div class="wrapper">
+	    <img class="logo" src="img/logo.png">
+			
+		<div class="center">
+			<h2>Inventory Management System</h2>
+			<a class="button" href="index.php">Login</a>
+			
+		</div>
+    </div>
+</body>
+</html>
