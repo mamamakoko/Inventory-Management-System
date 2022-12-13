1eaa53469fad2b79cb8fc9a6b857c8f5b62b192b
hfuribun <118652698+hfuribun@users.noreply.github.com>
2022-12-14 02:14:15 +0800

    Add files via upload
    add edit no. of stock

---
+++ modification.php
@@ -0,0 +1,87 @@
+<?php
+// including the database connection file
+include_once("../source/database.php");
+
+if (isset($_POST['update'])) {
+
+	$id = mysqli_real_escape_string($connection, $_POST['id']);
+
+	$name = mysqli_real_escape_string($connection, $_POST['name']);
+	$date = mysqli_real_escape_string($connection, $_POST['date']);
+	$stocks = mysqli_real_escape_string($connection, $_POST['stocks']);
+
+	// checking empty fields
+	if (empty($name) || empty($date)) {
+
+		if (empty($name)) {
+			echo "<font color='red'>Name field is empty.</font><br/>";
+		}
+
+		if (empty($date)) {
+			echo "<font color='red'>Date field is empty.</font><br/>";
+		}
+	} else {
+		//updating the table
+		$result = mysqli_query($connection, "UPDATE inventory SET itemName='$name',date='$date', stocks='$stocks' WHERE itemID=$id");
+
+		//redirectig to the display page. In our case, it is index.php
+		header("Location: ../dashboard.php");
+		exit;
+	}
+}
+
+if (isset($_POST['cancel'])) {
+
+	header("Location: ../dashboard.php");
+	exit;
+}
+
+?>
+<?php
+//getting id from url
+$id = $_GET['updateID'];
+
+//selecting data associated with this particular id
+$result = mysqli_query($connection, "SELECT * FROM inventory WHERE itemID=$id");
+
+while ($row = mysqli_fetch_array($result)) {
+	$name = $row['itemName'];
+	$date = $row['date'];
+	$stocks = $row['stocks'];
+}
+?>
+<html>
+
+<head>
+	<title>Edit Data</title>
+	<link href="../source/modification.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+	<form class="modification-content" method="post" action="modification.php">
+		<table>
+			<tr>
+				<td>Product Name</td>
+				<td><input type="text" name="name" value="<?php echo $name; ?>" class="mod-btn"></td>
+			</tr>
+			<tr>
+				<td>Date Added</td>
+				<td><input type="text" name="date" value="<?php echo $date; ?>" class="mod-btn"></td>
+			</tr>
+			<tr>
+				<td>No. of stocks</td>
+				<td><input type="text" name="stocks" value="<?php echo $stocks; ?>" class="mod-btn"></td>
+			</tr>
+			
+
+			<tr>
+				<td><input type="hidden" name="id" value=<?php echo $_GET['updateID']; ?>></td>
+				<td><input type="submit" name="update" value="Update"></td>
+				<td><input type="submit" name="cancel" value="Cancel" id="mod-cancel"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+
+</html>
